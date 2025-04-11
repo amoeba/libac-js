d6f79351ed79f91e470f71546a6c24e02a72552e
Bryce Mecum <petridish@gmail.com>
2025-04-10 19:02:55 -0700

    break out enums

---
+++ src/dat/enums/MagicSchool.ts
@@ -0,0 +1,8 @@
+export enum MagicSchool {
+  None = 0,
+  WarMagic = 1,
+  LifeMagic = 2,
+  ItemEnchantment = 3,
+  CreatureEnchantment = 4,
+  VoidMagic = 5,
+}
---
+++ src/dat/enums/SpellType.ts
@@ -0,0 +1,18 @@
+export enum SpellType {
+  Undef,
+  Enchantment,
+  Projectile,
+  Boost,
+  Transfer,
+  PortalLink,
+  PortalRecall,
+  PortalSummon,
+  PortalSending,
+  Dispel,
+  LifeProjectile,
+  FellowBoost,
+  FellowEnchantment,
+  FellowPortalSending,
+  FellowDispel,
+  EnchantmentProjectile
+}
